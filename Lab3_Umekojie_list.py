1f035951ee1bbf28e483a600378f9c2e9e687cf2
Josephine Cattell <josieohio7967@gmail.com>
2026-02-01 00:44:37 -0500

    add str

--- Lab3_Umekojie_list.py
+++ Lab3_Umekojie_list.py
@@ -3,6 +3,6 @@
 ### This program will print the total items on my list and sort them alphabetically.
 ### 1/30/26
 
-my_list = ["tops", "bottoms", "swimsuit", "shoes", "socks", "sweatshirts","bathrobe", "pj's", "underwear", "glasses"]
+my_list[str] = ["tops", "bottoms", "swimsuit", "shoes", "socks", "sweatshirts","bathrobe", "pj's", "underwear", "glasses"]
 print ("my list has ", len(my_list), "items.")
 print (sorted(my_list))
