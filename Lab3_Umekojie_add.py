98f2542aac93571100c459435e72a6f97cb54735
Josephine Cattell <josieohio7967@gmail.com>
2026-02-01 01:29:28 -0500

    add impotrt add

--- Lab3_Umekojie_add.py
+++ Lab3_Umekojie_add.py
@@ -1,7 +1,7 @@
 ### Add to list
 ### Josephine Cattell
 ### This program will add 5 items to the list
-### 1/30/26
+### 1/31/26
 from Lab3_Umekojie_list import my_list
 
 my_list.append ("purse")
---
+++ Lab3_Umekojie_replace.py
@@ -0,0 +1,5 @@
+### Replace an item in a list
+### Josephine Cattell
+### This program will import the list from add and relace using slice notation.
+### 2/1/26
+from Lab3_Umekojie_add import my_list
