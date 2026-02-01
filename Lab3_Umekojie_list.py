4c6601aa390a959ef66e80076dc1b3d8731bced8
Josephine Cattell <josieohio7967@gmail.com>
2026-01-31 23:34:12 -0500

    add list

--- Lab3_Umekojie_list.py
+++ Lab3_Umekojie_list.py
@@ -2,3 +2,5 @@
 ### Josephine Cattell
 ### This program will print the total items on my list and sort them alphabetically.
 ### 1/30/26
+
+my_list = ["tops", "bottoms", "swimsuit", "shoes", "socks", "sweatshirts","bathrobe", "pj's", "underwear", "glasses"]
